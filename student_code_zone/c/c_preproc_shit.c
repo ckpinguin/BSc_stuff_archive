d64b22acd787c1fb4da12dc0e76d8ee545866bc0
Christof Kälin <christof.kaelin@protonmail.com>
2023-12-12 13:04:53 +0100

    div. old stuff added

---
+++ student_code_zone/c/c_preproc_shit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define o scanf
+#define O printf
+#define ooh int
+#define OO "%d"
+#define Oho {
+#define oho }
+#define Oh main
+#define hohoho o##0
+#define oh *
+#define ho (
+#define Ho )
+#define harhar ;
+
+
+Oh ho Ho Oho ooh hohoho harhar o ho OO, &hohoho Ho harhar 
+O ho OO, hohoho oh hohoho oh hohoho Ho harhar oho
+
+
